957bf890f12aeaabe276cb748de850bbed9197b2
Hally Zhou <haorui2002@gmail.com>
2020-02-01 16:09:15 -0800

    IndexError solved

--- statsReader.py
+++ statsReader.py
@@ -7,19 +7,19 @@ with open('record.txt') as f:
     lines = [line.rstrip() for line in f]
     print(lines)
 f.close()
-print(len(lines))
 
-
-for x in range(len(lines)):
-    current = lines[x]
+i = 0
+linesLen = len(lines)
+for x in range(linesLen):
+    current = lines[i]
     try:
         if current[0] == 'i':
-            headingList.append(lines.pop(x))
-            x -= 1
+            headingList.append(lines.pop(i))
+            i -= 1
     except IndexError:
         print("current line is blank")
     print(headingList)
-
+    i += 1
 
 print(headingList)
 print(lines)
